f371d1fb04cd58b445ed2603c7fb9ef736717ce1
Chris Watts <cwatts@simplymeasured.com>
2015-05-27 22:29:15 -0700

    Added disqus Moved older posts to drafts

---
+++ assets/js/disqus.js
@@ -0,0 +1,11 @@
+/* * * CONFIGURATION VARIABLES * * */
+// Required: on line below, replace text in quotes with your forum shortname
+var disqus_shortname = 'Learning Ember';
+
+/* * * DON'T EDIT BELOW THIS LINE * * */
+(function () {
+    var s = document.createElement('script'); s.async = true;
+    s.type = 'text/javascript';
+    s.src = '//' + disqus_shortname + '.disqus.com/count.js';
+    (document.getElementsByTagName('HEAD')[0] || document.getElementsByTagName('BODY')[0]).appendChild(s);
+}());
